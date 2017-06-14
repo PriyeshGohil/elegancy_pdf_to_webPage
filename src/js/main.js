00b143b4cb32bdc8740dd37628c7d9d5213760fd
Priyesh Gohil <priyesh114@hotmail.co.uk>
2017-06-14 16:10:54 +0100

    menu toggle js code

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,11 @@ import '../css/main.scss'
 import {slider} from './slider.js'
 
 slider();
+
+// mobile menu toggle
+const menuBtn = document.querySelector('#menuBtn');
+const menuEl = document.querySelector('.header__menu');
+
+menuBtn.addEventListener('click', function() {
+    menuEl.classList.toggle('header__menu--open');
+});
